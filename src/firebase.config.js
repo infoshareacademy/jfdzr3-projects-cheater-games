40867e3e9d160463a709d25196d8e3f4d0c50fef
Przemek <przemo247@outlook.com>
2021-06-16 22:33:37 +0200

    Added firebase to package.json, added firebase config file to src folder

---
+++ src/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBin9gkJBodI-oJS4sH1kIesyIy8Xn-B2U',
+  authDomain: 'monster-hunt-v1.firebaseapp.com',
+  projectId: 'monster-hunt-v1',
+  storageBucket: 'monster-hunt-v1.appspot.com',
+  messagingSenderId: '5712090032',
+  appId: '1:5712090032:web:cf39258621713586d7bc75',
+};
