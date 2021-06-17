2670a858fc49080304a5fd061cefcc185cb0d055
Przemek <przemo247@outlook.com>
2021-06-17 21:35:20 +0200

    Adding completed positioning of user stats, adding points left to the UI

--- src/stats/Stats.js
+++ src/stats/Stats.js
@@ -1,6 +1,7 @@
 import Heading from './Heading';
 import Wrapper from './Wrapper';
 import Stat from './Stat';
+import './Stats.css';
 
 const stats = {
   str: 0,
@@ -13,15 +14,16 @@ const stats = {
 
 export default function Stats() {
   return (
-    <>
+    <main className="main">
       <Heading />
       <Wrapper>
+        <div className="user-stat__left">Pozostało punktów: {stats.left}</div>
         <Stat title="Siła" statAbr="str" statPoints={stats.str} />
         <Stat title="Zręczność" statAbr="agi" statPoints={stats.agi} />
         <Stat title="Wytrzymałość" statAbr="tough" statPoints={stats.tough} />
         <Stat title="Inteligencja" statAbr="int" statPoints={stats.int} />
         <Stat title="Spostrzegawczość" statAbr="perc" statPoints={stats.perc} />
       </Wrapper>
-    </>
+    </main>
   );
 }
