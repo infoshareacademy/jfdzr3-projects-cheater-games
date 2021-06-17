1d3c5137b65098326d2a8077188c907c79c9a8f2
Przemek <przemo247@outlook.com>
2021-06-17 23:11:04 +0200

    Adding useEffect hook  with database call to fetch stats basing on user id number

--- src/stats/Stats.js
+++ src/stats/Stats.js
@@ -1,29 +1,51 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import firebaseApp from '../firebaseConfig';
 import Heading from './Heading';
 import Wrapper from './Wrapper';
 import Stat from './Stat';
 import './Stats.css';
+import { useState, useEffect } from 'react';
 
-const stats = {
-  str: 0,
-  agi: 0,
-  tough: 0,
-  int: 0,
-  perc: 0,
-  left: 10,
-};
+const db = firebase.firestore();
+
+// Test
+const id = 'test-user';
 
 export default function Stats() {
+  const [points, setPoints] = useState({});
+  useEffect(() => {
+    db.collection('stats')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          const stats = doc.data();
+          setPoints(stats);
+        } else {
+          // doc.data() will be undefined in this case
+          console.log('No such document!');
+        }
+      })
+      .catch((error) => {
+        console.log('Error getting document:', error);
+      });
+  }, {});
   return (
     <main className="main">
       <Heading />
       <Wrapper>
-        <div className="user-stat__left">Pozostało punktów: {stats.left}</div>
-        <Stat title="Siła" statAbr="str" statPoints={stats.str} />
-        <Stat title="Zręczność" statAbr="agi" statPoints={stats.agi} />
-        <Stat title="Wytrzymałość" statAbr="tough" statPoints={stats.tough} />
-        <Stat title="Inteligencja" statAbr="int" statPoints={stats.int} />
-        <Stat title="Spostrzegawczość" statAbr="perc" statPoints={stats.perc} />
-        <button class="btn">Zatwierdź</button>
+        <div className="user-stat__left">Pozostało punktów: {points.left}</div>
+        <Stat title="Siła" statAbr="str" statPoints={points.str} />
+        <Stat title="Zręczność" statAbr="agi" statPoints={points.agi} />
+        <Stat title="Wytrzymałość" statAbr="tough" statPoints={points.tough} />
+        <Stat title="Inteligencja" statAbr="int" statPoints={points.int} />
+        <Stat
+          title="Spostrzegawczość"
+          statAbr="perc"
+          statPoints={points.perc}
+        />
+        <button className="btn">Zatwierdź</button>
       </Wrapper>
     </main>
   );
