79382920129732cc846b0738236c79128cebd7c3
Przemek <przemo247@outlook.com>
2021-06-18 23:00:24 +0200

    Add return to StatsProvider function

--- src/stats/StatsProvider.js
+++ src/stats/StatsProvider.js
@@ -6,22 +6,23 @@ const id = 'test-user';
 
 export const StatsContext = createContext();
 
-export function StatsProviderprops {
-    const [points, setPoints] = useState({});
-    useEffect(() => {
-      db.collection('stats')
-        .doc(id)
-        .get()
-        .then((doc) => {
-          if (doc.exists) {
-            const stats = doc.data();
-            setPoints(stats);
-          } else {
-            console.log('No such document!');
-          }
-        })
-        .catch((error) => {
-          console.log('Error getting document:', error);
-        });
-    }, {});
+export function StatsProvider(props) {
+  const [points, setPoints] = useState({});
+  useEffect(() => {
+    db.collection('stats')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          const stats = doc.data();
+          setPoints(stats);
+        } else {
+          console.log('No such document!');
+        }
+      })
+      .catch((error) => {
+        console.log('Error getting document:', error);
+      });
+  }, {});
+  return <StatsContext.Provider>{props.children}</StatsContext.Provider>;
 }
