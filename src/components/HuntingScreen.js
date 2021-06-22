62979177b45c63fc3d09106a6c4f1a0a7aef38bb
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-06-22 21:49:43 +0200

    small fixes to improve code

--- src/components/HuntingScreen.js
+++ src/components/HuntingScreen.js
@@ -1,46 +1,58 @@
 import React from "react";
+import { MainMenu } from "./MainMenu";
+import { Link } from "react-router-dom";
 
 export const HuntingScreen = () => {
   return (
     <>
-      <div className="hunting_screen">
-        <div className="hunting_screen-title">
-          <h2>Polowanie</h2>
-        </div>
-        <div className="hunting_screen-description">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-        </div>
-        <div className="hunting_screen-main">
-          <p>Poziom trudności: </p>
-          <div className="hunting_screen--choose_level">
-            <input type="radio" name="level" value="easy" />
-            <label>Łatwy</label>
-            <input type="radio" name="level" value="mid" />
-            <label>Średni</label>
-            <input type="radio" name="level" value="hard" />
-            <label>Trudny</label>
+      <div className="content">
+        <MainMenu />
+        <main className="main__section">
+          <div className="hunting__screen-title">
+            <h2>Polowanie</h2>
           </div>
-          <div className="hunting_screen--level_description">
-            <h3>Idź na polowanie</h3>
+          <div className="hunting__screen-description">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore
+              nisi ut aliquip ex ea commodo consequat.
             </p>
           </div>
-          <div className="hunting_screen--choose_ways">
-            <a href="#">LEWO</a>
-            <a href="#">W GÓRĘ</a>
-            <a href="#">W PRAWO</a>
+          <div className="hunting__screen-main">
+            <h4>Poziom trudności: </h4>
+            <div className="hunting__screen--choose_level">
+              <label>
+                Łatwy
+                <input type="radio" name="level" value="easy" />
+              </label>
+              <label>
+                Średni
+                <input type="radio" name="level" value="mid" />
+              </label>
+              <label>
+                Trudny
+                <input type="radio" name="level" value="hard" />
+              </label>
+            </div>
+            <div className="hunting__screen--level_description">
+              <h3>Idź na polowanie</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore
+              </p>
+            </div>
+            <div className="hunting__screen--choose_ways">
+              <Link to="/">LEWO</Link>
+              <Link to="/">W GÓRĘ</Link>
+              <Link to="/">W PRAWO</Link>
+            </div>
           </div>
-        </div>
+        </main>
+        <aside className="advertising"></aside>
       </div>
     </>
   );
