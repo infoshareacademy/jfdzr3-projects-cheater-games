4067f142e6fe597e08814336cfc44990cbabc355
Aga Skorupa <aga.skorupa@yahoo.pl>
2021-06-23 01:21:05 +0200

    Unused imports and variables fixed

--- src/global-chat/Message.js
+++ src/global-chat/Message.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './Message.css';
-import firebaseApp from '../firebaseConfig';
-import { db } from '../firebaseConfig';
 
 function Message({username, message}) {
     return (
         <div className="message_view">
             <h2>{message.username}: {message.text}</h2>
-
         </div>
-
     )
-}
+};
 
 export default Message;
--- src/global-chat/global-chat.js
+++ src/global-chat/global-chat.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import "./global-chat.css";
 import Message from "./Message";
-import firebaseApp from "../firebaseConfig";
 import { db } from "../firebaseConfig";
 import firebase from "firebase";
 import { auth } from "../firebaseConfig";
@@ -11,12 +10,7 @@ export function GlobalChat() {
 
     const [input, setInput] = useState('');
     const [messages, setMessages] = useState([]);
-    // const [username, setUsername] = useState('');
-
-    const user = auth.currentUser;
-    console.log(user);
-
-    //Following all changes in messages database, returns object {username: "", text: "" .....}
+    
     useEffect(() => {
         db.collection('messages')
         .orderBy('time', 'desc')
@@ -31,9 +25,6 @@ export function GlobalChat() {
     
     const { displayName, uid, } = auth.currentUser;
         
-    console.log(input);
-    console.log(messages);
-    
     const sendMessage = event => {
         event.preventDefault();
         
@@ -42,8 +33,6 @@ export function GlobalChat() {
             time: firebase.firestore.FieldValue.serverTimestamp(),
             username: displayName,
             uid: uid,
-            // photoURL: photoURL
-
         });
         setMessages([...messages, {username: displayName, text: input}]);
         setInput('');
@@ -52,26 +41,21 @@ export function GlobalChat() {
     return (
     <>
         <div className="chat">
-        <h2>Jesteś zalogowany jako: {displayName}</h2>
+            <h2>Jesteś zalogowany jako: {displayName}</h2>
 
-            {/* using form and button type="submit" to allow sending messages by clicking Enter */}
             <form>
 
-            <input placeholder="Wpisz wiadomość...."  value={input} onChange={event => setInput(event.target.value)}></input>
-            <button disabled={!input} type="submit"onClick={sendMessage}>Wyślij</button>
+                <input placeholder="Wpisz wiadomość...."  value={input} onChange={event => setInput(event.target.value)}></input>
+                <button disabled={!input} type="submit"onClick={sendMessage}>Wyślij</button>
 
             </form>
 
             {
-                messages.map(message => {
-                    return (
-                    <Message username={displayName} message={message}/>
-                    )
+            messages.map((message, index) => {
+            return (<Message key={message.time} username={displayName} message={message}/>)
                 })
-            }
-
-
+            };
         </div>
     </>    
     )
-} 
+}; 
