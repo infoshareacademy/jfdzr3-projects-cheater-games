7f789be8e2d4113744eac908bb0724e1a4134e3d
Marcin Tomkiewicz <marc.tomk@gmail.com>
2021-06-24 18:40:39 +0200

    Show full menu only if race has been chosen

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Logout } from "../auth/Logout";
 import { Link } from "react-router-dom";
+import { useUser } from "../hooks/useUser";
 
 export const MainMenu = () => {
+  const user = useUser();
+
   return (
     <nav className="nav">
       <ul>
         <li>
           <Logout />
         </li>
+        {user !== null && user?.race !== undefined ? (
+          <>
         <li>Widok postaci</li>
         <li>Statystyki</li>
         <li>Quest</li>
@@ -17,7 +22,11 @@ export const MainMenu = () => {
           <Link to="/hunt" style={{ textDecoration: "none", color: "inherit" }}>
             Idź na polowanie
           </Link>
-        </li>
+        </li> 
+        </>
+        ) : (
+          <li></li>
+        )}
       </ul>
     </nav>
   );
