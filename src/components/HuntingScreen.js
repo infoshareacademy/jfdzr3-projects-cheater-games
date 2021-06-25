4acfdd657a85d73d0a599d908839131dc0d1e245
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-06-25 21:08:54 +0200

    added new functionality to HuntingScreen like on clicking input radio button shows the description of the hunting place assigned to that button. Added Hover effect to the arrows and isntalled new HoverImage library

--- src/components/HuntingScreen.js
+++ src/components/HuntingScreen.js
@@ -1,32 +1,47 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import HoverImage from "react-hover-image";
 
 const description = [
   {
-    id: 1,
-    name: "easy",
-    desc: "Praesent ac odio ut nulla semper faucibus. Nam vel molestie lorem. Curabitur vulputate viverra pretium. Suspendisse velit ipsum, dapibus vitae blandit et, porta eget libero. Pellentesque eros mi, tincidunt ac suscipit eget, congue ut libero.",
+    name: "Łatwy",
+    descript: `Praesent ac odio ut nulla semper faucibus. Nam vel molestie lorem.
+    Curabitur vulputate viverra pretium. Suspendisse velit ipsum, dapibus vitae
+    blandit et, porta eget libero.Pellentesque eros mi, tincidunt ac suscipit eget, congue ut libero.`,
   },
   {
-    id: 2,
-    name: "mid",
-    desc: `Ut lobortis eleifend arcu, sed ornare mi molestie varius. Sed vel facilisis mi, non hendrerit arcu. Ut scelerisque, diam ut gravida convallis, 
-    ligula arcu laoreet dui, laoreet malesuada ex turpis quis sem.Mauris vitae massa eu tortor volutpat cursus ut auctor quam.
+    name: "Średni",
+    descript: `Ut lobortis eleifend arcu, sed ornare mi molestie varius. Sed vel facilisis mi,
+    non hendrerit arcu. Ut scelerisque, diam ut gravida convallis,
+    ligula arcu laoreet dui, laoreet malesuada ex turpis quis sem.Mauris vitae massa eu tortor
+    olutpat cursus ut auctor quam.
     Proin nisl tellus, venenatis non magna in, eleifend dapibus ipsum.Nullam eget elementum turpis.`,
   },
   {
-    id: 3,
-    name: "hard",
-    desc: `unc imperdiet auctor dapibus. Suspendisse dolor metus, efficitur ac diam eget, porttitor 
+    name: "Trudny",
+    descript: `unc imperdiet auctor dapibus. Suspendisse dolor metus, efficitur ac diam eget, porttitor 
     pellentesque justo.Donec in posuere turpis.Ut non ligula nunc.Mauris mollis ipsum ornare dolor pulvinar vulputate.V
-    estibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam lorem nisi, pretium sit amet sem et,
+    estibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+    Nullam lorem nisi, pretium sit amet sem et,
     sodales sodales mauris.`,
   },
 ];
 
 export const HuntingScreen = () => {
-  const [currentLevelId, setCurrentLevel] = useState("easy");
-  const currentLevel = description.find((desc) => desc.id === currentLevel);
+  const [currentLevelId, setCurrentLevel] = useState("");
+  const currentLevel = description.find((desc) => desc.name === currentLevelId);
+
+  const ifEmpty = () => {
+    if (currentLevel === undefined) {
+      return (
+        <p style={{ color: "red", fontSize: "20px" }}>
+          Wybierz poziom trudności polowania wojowniku!
+        </p>
+      );
+    } else {
+      return <p> {currentLevel.descript}</p>;
+    }
+  };
   return (
     <>
       <div className="content">
@@ -43,29 +58,64 @@ export const HuntingScreen = () => {
             </p>
           </div>
           <div className="hunting__screen-main">
-            <h4>Poziom trudności: </h4>
+            <h3>Poziom trudności: </h3>
             <div className="hunting__screen--choose_level">
-              <label>
-                Łatwy
-                <input type="radio" name="level" value="easy" />
-              </label>
-              <label>
-                Średni
-                <input type="radio" name="level" value="mid" />
-              </label>
-              <label>
-                Trudny
-                <input type="radio" name="level" value="hard" />
-              </label>
+              {description.map((desc) => (
+                <label>
+                  {desc.name}
+                  <input
+                    type="radio"
+                    name="level"
+                    key={desc.name}
+                    onClick={() => setCurrentLevel(desc.name)}
+                  ></input>
+                </label>
+              ))}
             </div>
             <div className="hunting__screen--level_description">
-              <h3>Idź na polowanie</h3>
-              <p>{currentLevel}</p>
+              <p>{ifEmpty()}</p>
             </div>
-            <div className="hunting__screen--choose_ways">
-              <Link to="/">LEWO</Link>
-              <Link to="/">W GÓRĘ</Link>
-              <Link to="/">W PRAWO</Link>
+            <div>
+              <h3>Wybierz ścieżkę polowania</h3>
+              <div className="hunting__screen--choose_ways">
+                <Link to="/">
+                  <HoverImage
+                    src={
+                      `${process.env.PUBLIC_URL}` + "/img/arrows/left-arrow.png"
+                    }
+                    hoverSrc={
+                      `${process.env.PUBLIC_URL}` +
+                      "/img/arrows/left-arrow-hover.png"
+                    }
+                    style={{ height: "70px", width: "70px" }}
+                  />
+                </Link>
+                <Link to="/">
+                  <HoverImage
+                    src={
+                      `${process.env.PUBLIC_URL}` + "/img/arrows/up-arrow.png"
+                    }
+                    hoverSrc={
+                      `${process.env.PUBLIC_URL}` +
+                      "/img/arrows/up-arrow-hover.png"
+                    }
+                    style={{ height: "70px", width: "70px" }}
+                  />
+                </Link>
+                <Link to="/">
+                  <HoverImage
+                    src={
+                      `${process.env.PUBLIC_URL}` +
+                      "/img/arrows/right-arrow.png"
+                    }
+                    hoverSrc={
+                      `${process.env.PUBLIC_URL}` +
+                      "/img/arrows/right-arrow-hover.png"
+                    }
+                    style={{ height: "70px", width: "70px" }}
+                  />
+                </Link>
+              </div>
             </div>
           </div>
         </main>
