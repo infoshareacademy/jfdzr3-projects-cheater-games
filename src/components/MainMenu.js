fbeb7f485f2361f8e6870b2fa8fc343a32ae0e15
Marcin Tomkiewicz <marc.tomk@gmail.com>
2021-06-29 12:56:11 +0200

    Links component created for rendering links depending on tables

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,43 +1,13 @@
 import React from "react";
-import { Logout } from "../auth/Logout";
-import { Link } from "react-router-dom";
-import { useUser } from "../hooks/useUser";
-import { HuntLink } from "./HuntLink";
-import { AdminLink } from "./AdminLink";
-import { link } from "fs-extra";
+import { Links } from "./Links"
+
 
 export const MainMenu = () => {
-  const user = useUser();
-
-  const links = [
-    <AdminLink />,
-    "Widok postaci",
-    "Statystyki",
-    <HuntLink />,
-    "Rynek",
-    <Logout />,
-  ];
-
-  const renderMenu = () => {
-    if (user?.role === "admin") {
-      links.map((el) => {
-        return <li>{el}</li>;
-      });
-    } else if (user?.role === "player") {
-      links.map((el, i) => {
-        if (i > 0) {
-          return <li>{el}</li>;
-        }
-      });
-    } else if (user?.role === "player" && user?.race === undefined) {
-      return <li>{links[links.length - 1]}</li>;
-    }
-  };
 
   return (
     <nav className="nav">
       <ul>
-        {renderMenu}
+        <Links />
         {/* <li>
           <Logout />
         </li>
