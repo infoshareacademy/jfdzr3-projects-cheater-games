154debf7a902c916fddc0ccc60e9ad78e30b4f33
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-07-04 23:43:55 +0200

    added Test component to check useItems works

--- src/components/store/useItems.js
+++ src/components/store/useItems.js
@@ -27,4 +27,82 @@ export const useItems = () => {
   return items;
 };
 
+export const Test = () => {
+  const items = useItems();
+  console.log(items);
+  items.map((item) => {
+    console.log(item.key);
+  });
+  items.map((item) => {
+    console.log(item.val);
+  });
+  items.map((item) => {
+    console.log(item.val.value);
+  });
+  items.map((item) => {
+    console.log(item.val.vit);
+  });
 
+  items.map((item) => {
+    console.log(item.id);
+  });
+
+  if (items === null) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <div className="item-list">
+        <h2>For Sale</h2>
+        {items &&
+          items.map((item, index) => (
+            <div key={index}>
+              <p>{item.key}</p>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+// export const Test = () => {
+
+//     const [items, setItems] = useState([])
+
+//     const isObject = (val) => {
+//         if (val === null) {
+//           return false;
+//         }
+//         return typeof val === "object";
+//       };
+
+//     useEffect(()=> {
+//         db.collection('items').onSnapshot((snapshot)=> {
+//             const newItems = snapshot.docs.map((doc)=>{
+//                 for (let property in doc.data()) {
+//                     console.log(doc.data()[property]);
+//                     console.log(property);
+//                     if (isObject(doc.data()[property])){
+
+//                     setItems({id: doc.id,key: property, val: doc.data()[property]});
+//                 }
+//             }
+//             })
+//             return newItems
+//         })
+//     },[])
+
+//     if (items !== null){
+
+//         console.log(items);
+//         console.log(items.key);
+//         console.log(items.val);
+//         console.log(items.id);
+
+//         // console.log(items.val.value);
+//         // console.log(items.val.vit);
+//     }
+
+//     return null
+// }
