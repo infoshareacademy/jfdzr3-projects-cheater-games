48af9d7aba0243196524d2d54d9b45ca34e909ae
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-07-05 12:51:34 +0200

    added hook useItems

---
+++ src/hooks/useItems.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { db } from "../../firebaseConfig";
+
+export const useItems = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    db.collection("items").onSnapshot((snapshot) => {
+      const newItems = [];
+      snapshot.docs.map((doc) => {
+        for (const property in doc.data()) {
+          newItems.push({
+            id: doc.id,
+            key: property,
+            val: doc.data()[property],
+          });
+        }
+        return newItems;
+      });
+      setItems(newItems);
+    });
+  }, []);
+  return items;
+};
