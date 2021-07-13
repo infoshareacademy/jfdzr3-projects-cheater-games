10a32be772abc037c4651f2cfffad98a00fcbea6
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-07-13 13:02:23 +0200

    styled-component

--- src/components/rynek/ItemsGrid.js
+++ src/components/rynek/ItemsGrid.js
@@ -7,10 +7,23 @@ import { TextBlock } from "./TextBlok";
 //     <div className="items-grid">{children}</div>
 //   </div>
 // );
+import styled from 'styled-components'
+const Wrapper = styled.div`
+display: grid;
+justify-content: space-between;
+align-items: flex-start;
+// grid-gap: 3px;
 
+width: 100%;
+grid-template-columns: repeat(4, 20%);
+border: 1px solid gray;
+box-shadow: 0 5px 10px rgba(0, 0, 0, 0.5);
+`
 export const ItemsGrid = ({ text, children }) => (
-  <div className="items-wrapper git s">
-    <TextBlock>{text}</TextBlock>
-    <div className="items-grid">{children}</div>
+   <> 
+  <div className="items-wrapper">
+   <TextBlock>{text}</TextBlock>
+    <Wrapper className="items-grid">{children}</Wrapper>
   </div>
+  </>
 );
--- src/components/rynek/StorePage.js
+++ src/components/rynek/StorePage.js
@@ -7,28 +7,36 @@ import { useItems } from "../../hooks/useItems";
 import { useUserItems } from "../../hooks/useUserItems";
 import { db } from "../../firebaseConfig";
 
+
+
+
+
 export const StorePage = () => {
   const user = useUser();
   const itemsCollectionPath = db.collection("items");
   const userItemsCollectionPath = db
-    .collection("users")
-    .doc(user?.uid)
-    .collection("armory");
+  .collection("users")
+  .doc(user?.uid)
+  .collection("armory");
   const itemsRef = useItems(itemsCollectionPath);
-  const userItemsRef = useUserItems(userItemsCollectionPath);
-
+  const userItemsRef = useUserItems(userItemsCollectionPath); 
+  
   return (
     <>
       <h1>Mirek Handlarz</h1>
       <div className="store-wrapper">
         <ItemsGrid text="Kup">
           <Items items={itemsRef} />
+          {/* <Items items={itemsRef} />
+          <Items items={itemsRef} /> */}
+
+
         </ItemsGrid>
         <Avatar />
         <ItemsGrid text="Sprzedaj">
           <Items items={userItemsRef} />
         </ItemsGrid>
       </div>
-    </>
+      </>
   );
 };
