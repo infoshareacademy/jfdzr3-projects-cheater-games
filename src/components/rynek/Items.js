181a8febd1cdd9cc122baa65b3426e6f53cc787d
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-07-13 15:01:40 +0200

    added material ui icons, and Chip with icon shoppingCard in Items.js

--- src/components/rynek/Items.js
+++ src/components/rynek/Items.js
@@ -1,31 +1,23 @@
-// import { db } from "../../firebaseConfig";
-// import { useItems } from "../../hooks/useItems";
-// import { useUserItems } from "../../hooks/useUserItems";
-
-
-
-// import { useUser } from "../../hooks/useUser";
 import styled from 'styled-components'
+import Chip from '@material-ui/core/Chip';
+import ShoppingCartRoundedIcon from '@material-ui/icons/ShoppingCartRounded';
 
 const ItemStyle = styled.div`
 height: max-content;
-// border: 1px solid gray;
+border: 1px solid lightgray;
 // white-space: nowrap;
 padding: 0 5px;
 min-width: 60px;
-min-height: 50px;
+min-height: 110px;
+max-height: fit-content;
 `
-
 export const Items = ({ items }) => {
+//   function IconChips() {
+//     return (
+//         <Chip icon={ <ShoppingCartRoundedIcon />} label={item.val.value} variant="outlined" />
+//     );
+//   }
   console.log(items);
-
-  // const user = useUser();
-  // const itemsRef = db.collection('items');
-  // const userItemsRef = db.collection('users').doc(user?.uid).collection('armory');
-  // const items = useItems(itemsRef);
-  // const userItems = useUserItems(userItemsRef)
-  // console.log(items);
-  // console.log(userItems);
 
   if (items.length === 0) {
     return <p>Loading</p>;
@@ -37,7 +29,8 @@ export const Items = ({ items }) => {
             <ItemStyle key={index}>
               <h5>{item.key}</h5>
               <h6 style={{marginTop: "5px"}}>gold: {item.val.value}</h6>
-            </ItemStyle>
+               <Chip label={item.val.value} icon={ <ShoppingCartRoundedIcon />}variant="outlined" size="small" style={{marginTop: "10px"}} />
+              </ItemStyle>
           ))}
       </>
     );
