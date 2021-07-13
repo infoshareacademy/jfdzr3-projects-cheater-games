55aca558c05dd7bab3575b25c301367397ea54a3
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-07-13 13:01:06 +0200

    added style component

--- src/components/rynek/Items.js
+++ src/components/rynek/Items.js
@@ -1,11 +1,24 @@
 // import { db } from "../../firebaseConfig";
 // import { useItems } from "../../hooks/useItems";
 // import { useUserItems } from "../../hooks/useUserItems";
+
+
+
 // import { useUser } from "../../hooks/useUser";
+import styled from 'styled-components'
+
+const ItemStyle = styled.div`
+height: max-content;
+// border: 1px solid gray;
+// white-space: nowrap;
+padding: 0 5px;
+min-width: 60px;
+min-height: 50px;
+`
 
 export const Items = ({ items }) => {
   console.log(items);
-  
+
   // const user = useUser();
   // const itemsRef = db.collection('items');
   // const userItemsRef = db.collection('users').doc(user?.uid).collection('armory');
@@ -14,21 +27,17 @@ export const Items = ({ items }) => {
   // console.log(items);
   // console.log(userItems);
 
-
-  if(items.length === 0) {
-    return (
-      <p>Loading</p>
-    )
-  }else {
-
+  if (items.length === 0) {
+    return <p>Loading</p>;
+  } else {
     return (
       <>
         {items &&
           items.map((item, index) => (
-            <div key={index}>
+            <ItemStyle key={index}>
               <h5>{item.key}</h5>
-              <h6>gold: {item.val.value}</h6>
-            </div>
+              <h6 style={{marginTop: "5px"}}>gold: {item.val.value}</h6>
+            </ItemStyle>
           ))}
       </>
     );
