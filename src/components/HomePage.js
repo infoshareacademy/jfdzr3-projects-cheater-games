9c95d6a010e00becab6416d57ac5bc3e79443935
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-07-15 11:33:45 +0200

    Fixed a problem with the login form not displaying after logging out. After logging out, the menu also does not appear

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,19 +3,22 @@ import React from "react";
 import { useUser } from "../hooks/useUser";
 import { SelectRace } from "./SelectRace";
 import { GlobalChat } from "../global-chat/global-chat";
+import {Login} from "../auth/Login"
 
 export const HomePage = () => {
   const user = useUser();
 
   return (
     <>
-   {user !== null ? (
+      {user !== null ? (
         <>
           <h1 className="welcome">Witaj w grze {user?.name}</h1>
           {user?.race === undefined ? <SelectRace /> : <GlobalChat />}
         </>
       ) : (
-        <></>
+        <>
+        <Login />
+        </>
       )}
     </>
   );
