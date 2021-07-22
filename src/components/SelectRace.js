4ba7823be7aba43b08996b369bee6f756523b09b
ossowskid <ossowski.dawid@gmail.com>
2021-07-22 22:14:26 +0200

    wip

--- src/components/SelectRace.js
+++ src/components/SelectRace.js
@@ -4,7 +4,87 @@ import { Link } from "react-router-dom";
 import { useUser } from "../hooks/useUser";
 import { db } from "../firebaseConfig";
 import s from "styled-components";
-
+const Slide = s.div`
+  background: black;
+  top: 25%;
+  position: absolute;
+  transition: all 0.5s;
+  transform-style: preserve-3d;
+  transition: all 0.5s ease-in-out;
+  :hover {
+    box-shadow: 0 0 1px rgba(255, 255, 255, 1);
+    ${(props) =>
+      props.highlight === 1 ? `transform: translateY(-10px) !important;` : ``}
+  }
+  :before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: -1px;
+    right: -1px;
+    bottom: 0;
+    left: -1px;
+    background: rgb(255,255,255);
+    background: 
+      linear-gradient(
+        rgba(255,255,255,1) 0%, 
+        rgba(0,0,0,0) 100%
+      );
+    background-position: 0 0;
+    background-size: cover;
+    transform: translateZ(-5px);
+    opacity: ${(props) => props.highlight};
+    transition: all 0.5s;
+  }
+`;
+const Button = s.button`
+  // display: flex;
+  padding: 20px;
+  background: blue;
+  `;
+const Image = s.img`
+  transition: all 0.5s;
+  display: block;
+  max-height: 350px;
+`;
+const Title = s.h2`
+  position: absolute;
+  color: white;
+  font-size: 30px;
+  text-shadow: 1px 1px 2px black;
+  transform: translateY(-100px) translateX(-20px);
+  transition: all 0.3s ease-in-out 0.5s;
+  ${(props) =>
+    props.show
+      ? `
+      opacity: 1;
+      ::after {
+        content: '';
+        border-bottom: 2px solid white;
+        width: 150px;
+        display: block;
+        margin-top: -5px;
+        position: absolute;
+        transition: width 1.5s;
+        z-index: -1;
+      }
+      `
+      : `
+      opacity: 0; 
+      transition: all 0.5s ease-in-out 0s; 
+      transform: translateY(-100px) translateX(-20px);
+      ::after {
+        content: '';
+        border-bottom: 2px solid white;
+        width: 0;
+        transition: width 2.5s;
+        z-index: -1;
+        display: block;
+        margin-top: -5px;
+        position: absolute;
+      }
+      `}
+`;
 export const SelectRace = () => {
   const getRaceFromDB = () => {
     return db
@@ -35,136 +115,27 @@ export const SelectRace = () => {
         return cards;
       });
   };
-
-  const Slide = s.div`
-  background: black;
-  top: 25%;
-  position: absolute;
-  transition: all 0.5s;
-  transform-style: preserve-3d;
-  transition: all 0.5s ease-in-out;
-
-  :hover {
-    box-shadow: 0 0 1px rgba(255, 255, 255, 1);
-    ${(props) =>
-      props.highlight === 1 ? `transform: translateY(-10px) !important;` : ``}
-  }
-
-  :before {
-    content: '';
-    display: block;
-    position: absolute;
-    top: -1px;
-    right: -1px;
-    bottom: 0;
-    left: -1px;
-    background: rgb(255,255,255);
-    background: 
-      linear-gradient(
-        rgba(255,255,255,1) 0%, 
-        rgba(0,0,0,0) 100%
-      );
-    background-position: 0 0;
-    background-size: cover;
-    transform: translateZ(-5px);
-    opacity: ${(props) => props.highlight};
-    transition: all 0.5s;
-  }
-`;
-
-  const Button = s.button`
-  // display: flex;
-  padding: 20px;
-  background: blue;
-  `;
-
-  const Image = s.img`
-  transition: all 0.5s;
-  display: block;
-  max-height: 350px;
-`;
-
-  const Title = s.h2`
-  position: absolute;
-  color: white;
-  font-size: 30px;
-  text-shadow: 1px 1px 2px black;
-  transform: translateY(-100px) translateX(-20px);
-  transition: all 0.3s ease-in-out 0.5s;
-
-  ${(props) =>
-    props.show
-      ? `
-      opacity: 1;
-      ::after {
-        content: '';
-        border-bottom: 2px solid white;
-        width: 150px;
-        display: block;
-        margin-top: -5px;
-        position: absolute;
-        transition: width 1.5s;
-        z-index: -1;
-      }
-      `
-      : `
-      opacity: 0; 
-      transition: all 0.5s ease-in-out 0s; 
-      transform: translateY(-100px) translateX(-20px);
-
-      ::after {
-        content: '';
-        border-bottom: 2px solid white;
-        width: 0;
-        transition: width 2.5s;
-        z-index: -1;
-        display: block;
-        margin-top: -5px;
-        position: absolute;
-      }
-      `}
-`;
-
   const positions = [
     { deg: 20, depth: -200, shading: 0.3, x: -100, highlight: 0 },
     { deg: 0, depth: 0, shading: 1, x: 0, highlight: 1 },
     { deg: -20, depth: -200, shading: 0.3, x: 100, highlight: 0 },
-    { deg: 0, depth: -20, shading: 1, x: 0, highlight: 1 },
+    { deg: 0, depth: -200, shading: 1, x: 0, highlight: 1 },
   ];
-
   const [races, setRaces] = useState([]);
-
   useEffect(() => {
     getRaceFromDB().then((racesFromDB) => {
       setRaces(racesFromDB);
     });
   }, []);
-
   const [currentSlide, setCurrentSlide] = useState(0);
-
   const getNextSlide = () => setCurrentSlide((x) => (x + 1) % races.length);
   const getPrevSlide = () =>
     setCurrentSlide((x) => (x - 1 + races.length) % races.length);
-
   // const slides = [...races, ...races, ...races].slice(
   //   currentSlide + races.length - 1,
   //   currentSlide + races.length + 2
   // );
-  const slides = races;
-
-  const getSlides = slides.map((slide) => ({
-    ...slide,
-  }));
-
   const user = useUser();
-
-  const updateRace = () => {
-    const { name } = slides[1];
-    db.collection("users").doc(user.uid).update({
-      race: name,
-    });
-  };
-
   const unitsMap = {
     str: { label: "Siła" },
     agi: { label: "Zręczność" },
@@ -178,21 +149,31 @@ export const SelectRace = () => {
     wood: { label: "Przyrost drewna", unit: "/h" },
     mat: { label: "Przyrost materiału", unit: "/h" },
   };
-  if (slides.length === 0) {
+  if (races.length === 0) {
     return <p>Loading...</p>;
   }
-
+  const dataIndex =
+    (((currentSlide + 1) % races.length) + races.length) % races.length;
+  console.log(dataIndex);
+  console.log(races[dataIndex].name);
+  const { bonus, name, description } = races[dataIndex];
+  const updateRace = (name) => {
+    db.collection("users").doc(user.uid).update({
+      race: name,
+    });
+  };
   return (
     <section className="select-race">
       <div className="wrapper">
         <div className="slider">
-          {getSlides.map((slide, i) => {
+          {races.map((slide, i) => {
             const d =
               (((currentSlide + i) % positions.length) + positions.length) %
               positions.length;
             const { deg, depth, shading, x, highlight } = positions[d];
             return (
               <Slide
+                key={slide.id}
                 highlight={highlight}
                 style={{
                   order: d,
@@ -201,7 +182,6 @@ export const SelectRace = () => {
               >
                 <Image
                   src={slide.src}
-                  key={slide.id}
                   alt={slide.name}
                   style={{ opacity: `${shading}` }}
                 />
@@ -213,13 +193,12 @@ export const SelectRace = () => {
       </div>
       <Button onClick={getPrevSlide}>Poprzedni</Button>
       <Button onClick={getNextSlide}>Następny</Button>
-
       <div className="wrapper__desc">
-        <h2 className="description race__name">{getSlides[1].name}</h2>
-        <div className="description">{getSlides[1].description}</div>
+        <h2 className="description race__name">{name}</h2>
+        <div className="description">{description}</div>
         <div className="bonus bonus__title">Bonus rasowy:</div>
         <div className="bonus">
-          {getSlides[1].bonus
+          {bonus
             .filter((bonus) => bonus.change !== 0)
             .map((bonus) => (
               <div key={bonus.name}>
@@ -234,10 +213,10 @@ export const SelectRace = () => {
           <Link
             to="/"
             style={{ textDecoration: "none", color: "inherit" }}
-            onClick={updateRace}
+            onClick={() => updateRace(name)}
           >
             <button className="btn btn-green margin-right-medium">
-              Wybierz rasę: {slides[1].name}
+              Wybierz rasę: {name}
             </button>
           </Link>
           <button className="btn btn-red">
