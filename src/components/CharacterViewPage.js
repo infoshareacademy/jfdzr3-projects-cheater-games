038d8e5dff5f9f48ec90169116c4cda37f087d2c
ossowskid <ossowski.dawid@gmail.com>
2021-07-27 21:58:37 +0200

    something done, but not working. Will try to fix it tomorrow

--- src/components/CharacterViewPage.js
+++ src/components/CharacterViewPage.js
@@ -1,31 +1,64 @@
 import s from "styled-components";
+import { GlobalChat } from "../global-chat/global-chat";
 import { db } from "../firebaseConfig";
+import { useUser } from "../hooks/useUser";
 
 const Wrapper = s.div`
 padding: 20px;
 display: flex;
 flex-flow: column;
-width: 1100px;
-height: 300px;
+width: 100%;
+height: 500px;
 border: 1px solid black;
 `;
 const HeaderText = s.h2`
+padding: 5px;
 margin: 0 auto;
 `;
 const CharacterMainBax = s.div`
+display: flex;
+flex-flow: row;
 width: 100%;
-height: 80%;
+height: 95%;
 background: tomato;
-
+`;
+const CharacterInformationBox = s.div`
+width: 50%;
+height: 100%;
+background: yellow;
+`;
+const CharacterAvatarBox = s.div`
+width: 50%;
+height: 100%;
+background: green;
 `;
 
 export const CharacterViewPage = () => {
+  const user = useUser();
+
+  const checkRace = (race) => {
+    const data = db
+      .collection("users")
+      .doc(user.uid)
+      .get(race.name)
+      .then((race) => {
+        return {
+          name: race.race,
+        };
+      });
+    console.log(data);
+  };
+
   return (
     <>
       <Wrapper>
         <HeaderText>Widok postaci</HeaderText>
-        <CharacterMainBax>asd</CharacterMainBax>
+        <CharacterMainBax>
+          <CharacterInformationBox />
+          <CharacterAvatarBox>{checkRace}</CharacterAvatarBox>
+        </CharacterMainBax>
       </Wrapper>
+      <GlobalChat />
     </>
   );
 };
