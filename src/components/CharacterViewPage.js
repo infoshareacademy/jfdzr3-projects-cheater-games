714293d95a18fddc52188e54d8d8f83c311f3600
ossowskid <ossowski.dawid@gmail.com>
2021-07-27 20:53:15 +0200

    wip

--- src/components/CharacterViewPage.js
+++ src/components/CharacterViewPage.js
@@ -1,8 +1,31 @@
+import s from "styled-components";
+import { db } from "../firebaseConfig";
+
+const Wrapper = s.div`
+padding: 20px;
+display: flex;
+flex-flow: column;
+width: 1100px;
+height: 300px;
+border: 1px solid black;
+`;
+const HeaderText = s.h2`
+margin: 0 auto;
+`;
+const CharacterMainBax = s.div`
+width: 100%;
+height: 80%;
+background: tomato;
+
+`;
+
 export const CharacterViewPage = () => {
-    return (
-      <>
-        <h1>Widok postaci</h1>
-      </>
-    );
-  };
-  
+  return (
+    <>
+      <Wrapper>
+        <HeaderText>Widok postaci</HeaderText>
+        <CharacterMainBax>asd</CharacterMainBax>
+      </Wrapper>
+    </>
+  );
+};
