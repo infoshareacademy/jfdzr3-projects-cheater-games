5d3d9b307cfaa02edaae41d98cd3a88f846e23c1
Przemek <przemo247@outlook.com>
2021-07-28 21:12:20 +0200

    Add link to chat in the navigation

--- src/components/Links.js
+++ src/components/Links.js
@@ -2,30 +2,25 @@ import React from "react";
 import { Logout } from "../auth/Logout";
 import { useUser } from "../hooks/useUser";
 import { HuntLink } from "../links/HuntLink";
-import { AdminLink } from "../links/AdminLink"
+import { AdminLink } from "../links/AdminLink";
 import { StatsLink } from "../links/StatsLink";
 import { CharacterViewLink } from "../links/CharacterViewLink";
 import { StoreLink } from "../links/StoreLink";
+import { ChatLink } from "../links/ChatLink";
 
 export const Links = () => {
   const user = useUser();
-
-  const adminLinks = [
-    <AdminLink />,
-    <CharacterViewLink />,
-    <StatsLink />,
-    <HuntLink />,
-    <StoreLink />,
-    <Logout />,
-  ];
 
   const userLinks = [
     <CharacterViewLink />,
     <StatsLink />,
     <HuntLink />,
     <StoreLink />,
+    <ChatLink />,
     <Logout />,
   ];
+
+  const adminLinks = [<AdminLink />, ...userLinks];
 
   if (user?.role === "admin") {
     return adminLinks.map((el, i) => {
