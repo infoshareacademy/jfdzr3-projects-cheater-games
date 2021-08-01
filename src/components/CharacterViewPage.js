c264dff017233040960c19d2543b7d8163fc0de9
ossowskid <ossowski.dawid@gmail.com>
2021-08-01 19:08:06 +0200

    wip

--- src/components/CharacterViewPage.js
+++ src/components/CharacterViewPage.js
@@ -1,6 +1,11 @@
 import s from "styled-components";
 import { GlobalChat } from "../global-chat/global-chat";
 import { useUser } from "../hooks/useUser";
+import { PropTypes } from "prop-types";
+import { CircularProgress } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+import { useState, useEffect } from "react";
 
 const Wrapper = s.div`
 padding: 20px;
@@ -10,61 +15,46 @@ width: 100%;
 height: 500px;
 border: 1px solid black;
 `;
-const HeaderText = s.h2`
-padding: 5px;
-margin: 0 auto;
-`;
+
 const CharacterMainBax = s.div`
 display: flex;
 flex-flow: row;
-width: 100%;
+justify-contenet: center;
+align-items: center;
+
 height: 95%;
-// background: tomato;
 `;
-const CharacterInformationBox = s.div`
-width: 50%;
-height: 100%;
-display:flex;
-justify-content: center;
-`;
-const CharacterAvatarBox = s.div`
-width: 50%;
-height: 100%;
-// background: green;
-`;
-const AvatarBoxContainer = s.div`
+const CharacterNameAndStatsBox = s.div`
 display: flex;
 flex-flow: column;
 justify-content: center;
-align-items: center;
-width: 100%;
-height: 100%;
-`;
-const Image = s.img`
-margin-top: 20px;
-height: 70%;
-// object-fit: contain;
-border-radius: 50% 50% 50% 50%;
-`;
-
-const RaceInformationText = s.p`
-font-size: 1.2rem;
-margin: 5px;
-`;
-
-const SecondWrapper = s.div`
+text-align: center;
 height: 100%;
 width: 50%;
-// background: yellow;
-justify-content: center;
-display: flex;
-flex-flow: column;`;
-
-const Stats = s.div`
-padding: 10px
 `;
-const StatsNameBolder = s.p`
-font-weight: bold
+
+const CharacterInformationBox = s.div`
+display:flex;
+flex-flow: column;
+justify-content: center;
+text-align: center;
+width: 50%;
+height: 100%;
+// background: tomato;
+`;
+
+const CharacterAvatarBox = s.div`
+display: flex;
+justify-contenet: center;
+alightn-items: center;
+width: 50%;
+height: 100%;
+// background: red;
+`;
+const Image = s.img`
+margin: auto;
+height: 70%;
+border-radius: 25% 25% 25% 25%;
 `;
 
 export const CharacterViewPage = () => {
@@ -72,18 +62,10 @@ export const CharacterViewPage = () => {
   const checkRaceToSetAvatar = () => {
     if (user?.race === "Krasnolud") {
       return (
-        <AvatarBoxContainer>
-          <Image
-            src={`${process.env.PUBLIC_URL}/img/races/Krasnolud.jpg`}
-            alt="Krasnolud"
-          />
-          <RaceInformationText>
-            <h3>{user?.name}</h3>
-          </RaceInformationText>
-          <RaceInformationText>
-            <h3>{user?.race}</h3>
-          </RaceInformationText>
-        </AvatarBoxContainer>
+        <Image
+          src={`${process.env.PUBLIC_URL}/img/races/Krasnolud.jpg`}
+          alt="Krasnolud"
+        />
       );
     }
     if (user?.race === "Elf") {
@@ -106,66 +88,87 @@ export const CharacterViewPage = () => {
     }
   };
 
+  function CircularProgressWithLabel(props) {
+    return (
+      <>
+        <p>Zdobyte doświadczenie:</p>
+        <Box position="relative" display="inline-flex">
+          <CircularProgress variant="determinate" size="70px" {...props} />
+          <Box
+            top={0}
+            left={0}
+            bottom={0}
+            right={0}
+            position="absolute"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography variant="caption" component="div" color="#00695f">
+              {`${user?.exp}XP`}
+            </Typography>
+          </Box>
+        </Box>
+      </>
+    );
+  }
+  CircularProgressWithLabel.propTypes = {
+    value: PropTypes.number.isRequired,
+  };
+
+  const CirculatStatistic = () => {
+    const [progress, setProgress] = useState(10);
+    useEffect(() => {
+      const timer = setInterval(() => {
+        setProgress((prevProgress) =>
+          prevProgress >= 100 ? 0 : prevProgress + 10
+        );
+      }, 800);
+      return () => {
+        clearInterval(timer);
+      };
+    }, []);
+    return <CircularProgressWithLabel value={progress} />;
+  };
   return (
     <>
       <Wrapper>
-        <HeaderText>Widok postaci</HeaderText>
+        <h2 className="character-overview-text">Widok postaci</h2>
         <CharacterMainBax>
           <CharacterInformationBox>
-            <SecondWrapper>
-              <Stats>
-                <StatsNameBolder>
-                  Poziom: <span>{user?.level}</span>
-                </StatsNameBolder>
+            <div>
+              <h2>Materiały:</h2>
+              <p>Złoto: {user?.resources.gold}</p>
+              <p>Drewno: {user?.resources.wood}</p>
+              <p>Materiały: {user?.resources.material}</p>
+            </div>
 
-                <StatsNameBolder>
-                  Zdobyte doświadczenie: <span>{user?.exp}</span>
-                </StatsNameBolder>
-              </Stats>
-
-              <Stats>
-                <h3>Materiały:</h3>
-                <StatsNameBolder>
-                  Złoto: <span>{user?.resources.gold}</span>
-                </StatsNameBolder>
-                <StatsNameBolder>
-                  Drewno: <span>{user?.resources.wood}</span>
-                </StatsNameBolder>
-                <StatsNameBolder>
-                  Materiały: <span>{user?.resources.material}</span>
-                </StatsNameBolder>
-              </Stats>
-
-              <Stats>
-                <h3>Statystyki</h3>
-                <StatsNameBolder>
-                  Zręczność: <span>{user?.stats.agi}</span>
-                </StatsNameBolder>
-                <StatsNameBolder>
-                  Inteligencja: <span>{user?.stats.int}</span>
-                </StatsNameBolder>
-                <StatsNameBolder>
-                  Spostrzegawczość: <span>{user?.stats.perc}</span>
-                </StatsNameBolder>
-                <StatsNameBolder>
-                  Szybkość: <span>{user?.stats.speed}</span>
-                </StatsNameBolder>
-                <StatsNameBolder>
-                  Siła: <span>{user?.stats.str}</span>
-                </StatsNameBolder>
-                <StatsNameBolder>
-                  Wytrzymałość: <span>{user?.stats.tough}</span>
-                </StatsNameBolder>
-                <StatsNameBolder>
-                  Żywotność: <span>{user?.stats.vit}</span>
-                </StatsNameBolder>
-                <StatsNameBolder>
-                  Pozotało do rozdzielenia: <span>{user?.stats.left}</span>
-                </StatsNameBolder>
-              </Stats>
-            </SecondWrapper>
+            <div>
+              <h2>Statystyki</h2>
+              <p>Zręczność: {user?.stats.agi}</p>
+              <p>Inteligencja: {user?.stats.int}</p>
+              <p>Spostrzegawczość: {user?.stats.perc}</p>
+              <p>Szybkość: {user?.stats.speed}</p>
+              <p>Siła: {user?.stats.str}</p>
+              <p>Wytrzymałość: {user?.stats.tough}</p>
+              <p>Żywotność: {user?.stats.vit}</p>
+              <p>Pozotało do rozdzielenia: {user?.stats.left}</p>
+            </div>
           </CharacterInformationBox>
           <CharacterAvatarBox>{checkRaceToSetAvatar()}</CharacterAvatarBox>
+          <CharacterNameAndStatsBox>
+            <p>
+              <h2>{user?.name}</h2>
+            </p>
+            <p>
+              <h2>{user?.race}</h2>
+            </p>
+            <div>
+              <p>Poziom: {user?.level}</p>
+
+              <CirculatStatistic />
+            </div>
+          </CharacterNameAndStatsBox>
         </CharacterMainBax>
       </Wrapper>
       <GlobalChat />
