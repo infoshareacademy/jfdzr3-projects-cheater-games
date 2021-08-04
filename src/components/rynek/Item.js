f6475d090150cf0cc0e70b65d096f9ddd26f7a86
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-08-04 14:28:38 +0200

    added an option that blocks purchases if the player does not have enough gold.

--- src/components/rynek/Item.js
+++ src/components/rynek/Item.js
@@ -2,39 +2,49 @@ import React from "react";
 import Chip from "@material-ui/core/Chip";
 import ShoppingCartRoundedIcon from "@material-ui/icons/ShoppingCartRounded";
 import { GiSwapBag } from "react-icons/gi";
+import { useCart } from "./CartContext";
+import { useUser } from "../../hooks/useUser";
+
 
 export const Item = ({ name, value, icon, onBuyClick, onSellClick }) => {
+const {getTotalPrice} = useCart();
+const totalPrice = getTotalPrice();
+const user = useUser();
+const userGold = user?.resources.gold;
+
   return (
     <>
       <h5>{name}</h5>
       <img src={icon} style={{ height: "70px" }} alt="" />
       {onSellClick && (
-        <Chip
-          label={value}
-          icon={<GiSwapBag style={{ color: "#0e79b2" }} />}
-          variant="outlined"
-          size="small"
-          style={{
-            fontWeight: "500",
-            color: "#0e79b2",
-            border: "transparent",
-          }}
-          onClick={() => onSellClick()}
-        />
+        <button onClick={() => onSellClick()} disabled={userGold < totalPrice}>
+          <Chip
+            label={value}
+            icon={<GiSwapBag style={{ color: "#0e79b2" }} />}
+            variant="outlined"
+            size="small"
+            style={{
+              fontWeight: "500",
+              color: "#0e79b2",
+              border: "transparent",
+            }}
+          />
+        </button>
       )}
       {onBuyClick && (
-        <Chip
-          label={value}
-          icon={<ShoppingCartRoundedIcon style={{ color: "green" }} />}
-          variant="outlined"
-          size="small"
-          style={{
-            fontWeight: "500",
-            color: "green",
-            border: "transparent",
-          }}
-          onClick={() => onBuyClick()}
-        />
+        <button onClick={() => onBuyClick()} disabled={value === 500}>
+          <Chip
+            label={value}
+            icon={<ShoppingCartRoundedIcon style={{ color: "green" }} />}
+            variant="outlined"
+            size="small"
+            style={{
+              fontWeight: "500",
+              color: "green",
+              border: "transparent",
+            }}
+          />
+        </button>
       )}
     </>
   );
