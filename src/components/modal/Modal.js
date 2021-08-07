7415ccdf98a38dcb116fc6dee827c0b74cfaa661
Przemek <przemo247@outlook.com>
2021-08-07 21:35:25 +0200

    Add working solution

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -5,9 +5,9 @@ import easy3 from "./monsters/easy3.jpg";
 import medium1 from "./monsters/medium1.jpg";
 import medium2 from "./monsters/medium2.jpg";
 import medium3 from "./monsters/medium3.jpg";
-import hard1 from "./monsters/hard3.jpg";
-import hard2 from "./monsters/hard1.jpg";
-import hard3 from "./monsters/hard2.jpg";
+import hard1 from "./monsters/hard1.jpg";
+import hard2 from "./monsters/hard2.jpg";
+import hard3 from "./monsters/hard3.jpg";
 
 const easyMonsters = [easy1, easy2, easy3];
 const mediumMonsters = [medium1, medium2, medium3];
@@ -28,7 +28,8 @@ export const Modal = ({ onClose, difficulty }) => {
   };
 
   useEffect(() => {
-    const randomNumber = Math.round(Math.random() * 3);
+    const randomNumber = Math.round(Math.random() * 2);
+    console.log(randomNumber);
     let drawnMonster, numClicks, time;
     if (difficulty === "easy") {
       drawnMonster = easyMonsters[randomNumber];
@@ -59,24 +60,52 @@ export const Modal = ({ onClose, difficulty }) => {
       clearInterval(interval);
       setGamePlaying(false);
     }
-  }, [gamePlaying, time]);
-  return (
-    <div className="modal">
-      <div className="modal__heading">
-        <h2>Walcz!</h2>
-        <span>
-          Pozostało ci: {time < 0 ? 0 : time / 1000} sekund i {numClicks}{" "}
-          kliknięć!
-        </span>
+  }, [time, gamePlaying]);
+
+  if (gamePlaying) {
+    return (
+      <div className="modal">
+        <div className="modal__heading">
+          <h2>Walcz!</h2>
+          <span>
+            Pozostało ci: {time < 0 ? 0 : time / 1000} sekund i {numClicks}{" "}
+            kliknięć!
+          </span>
+        </div>
+        <div className="modal__content">
+          <img
+            src={monsterImg}
+            style={{ height: "450px" }}
+            alt=""
+            onClick={handleOnClick}
+          />
+        </div>
       </div>
-      <div className="modal__content">
-        <img
-          src={monsterImg}
-          style={{ height: "450px" }}
-          alt=""
-          onClick={handleOnClick}
-        />
+    );
+  } else if (!gamePlaying && time > 0 && numClicks === 0) {
+    return (
+      <div className="modal">
+        <div className="modal__content">
+          <div className="modal__heading">
+            <h1>Pokonałeś potwora!</h1>
+            <span>Wybierz swoją nagrodę!</span>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else if (!gamePlaying && time <= 0 && numClicks > 0) {
+    return (
+      <div className="modal">
+        <div className="modal__content">
+          <div className="modal__heading">
+            <h1>Porażka!</h1>
+            <span>Zamknij okno i spróbój ponownie!</span>
+          </div>
+          <button className="btn btn-red" onClick={onClose}>
+            Zamknij
+          </button>
+        </div>
+      </div>
+    );
+  }
 };
