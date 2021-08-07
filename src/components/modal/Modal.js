260b0be36efb3a569912092c88dd6fd768940e50
Przemek <przemo247@outlook.com>
2021-08-07 17:06:46 +0200

    Add Modal component

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,1 +1,7 @@
-export const Modal = ({ onClose }) => {};
+export const Modal = () => {
+  return (
+    <div className="modal">
+      <div className="modal__content">Lorem ipsum</div>
+    </div>
+  );
+};
